7b4d149fd85258b410d410b5284b14a04c0c1886
Wilson.ST <wilsonthehungryman@gmail.com>
2017-03-18 20:37:57 +0000

    Coded controller actions.

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,10 +1,14 @@
+#
 class FinderController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def alphabetized
+    @customers = Customer.order(:name)
   end
 
   def missing_email
+    @customers = Customer.where(email: [nil, ''])
   end
 end
